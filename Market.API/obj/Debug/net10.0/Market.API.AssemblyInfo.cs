f5905e02907db12f7fb29748b8f187ab6cb2134d
Akbarjon <akbarjonfayzulloev585@mail.com>
2025-12-04 13:06:01 +0500

    v1.0

--- Market.API/obj/Debug/net10.0/Market.API.AssemblyInfo.cs
+++ Market.API/obj/Debug/net10.0/Market.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Market.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8786b7c673fd7154f60d3dd19746b8713556901b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d651cc502065c09446255ca389298e43316e853c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Market.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Market.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Market.Infrastructure/obj/Debug/net10.0/Market.Infrastructure.AssemblyInfo.cs
+++ Market.Infrastructure/obj/Debug/net10.0/Market.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Market.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d49d1fbb5c1c6b47cd5bc37be67d19c676f538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d651cc502065c09446255ca389298e43316e853c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Market.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Market.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
